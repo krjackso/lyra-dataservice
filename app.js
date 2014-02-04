ba768652465628849d6ff34b7be4d708b86abddc
Keilan Jackson <keilan.r.jackson@gmail.com>
2014-02-03 18:04:57 -0700

    hello world added

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var mysql = require('mysql');
+
+var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST;
+var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT;
+var mysqlUser = process.env.OPENSHFIT_MYSQL_DB_USERNAME;
+var mysqlPass = process.env.OPENSHIFT_MYSQL_DB_PASSWORD;
+
+// Config
+var connection = mysql.createConnection({
+	host		: mysqlHost,
+	port		: mysqlPort, 
+	user		: mysqlUser,
+	password	: mysqlPassword,
+	database	: 'data',
+});
+
+app.use(express.logger('dev'));
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.get('/hello', function(req, res) {
+	res.send('Hello World');
+});
